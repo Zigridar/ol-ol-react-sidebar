347cba70da88133f52a346e1c4183b69d5f197e4
zigridar <safonenko.max@yandex.ru>
2021-04-12 02:21:36 +0500

    add build script

--- index.js
+++
@@ -1,2 +0,0 @@
-import Sidebar from './src/Sidebar'
-export default Sidebar
--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,6 +1,41 @@
 import React, {useEffect, useRef} from 'react'
 import './Sidebar.css'
-import {SideBarProps, TabPaneProps, TabProps} from './index'
+/** Sidebar position */
+export type SidebarPosition = 'left' | 'right'
+
+/** Tab props for user */
+export interface ITabProps {
+    id: string
+    icon?: React.ReactNode
+}
+
+/** Private Sidebar Tar props */
+export interface TabProps extends ITabProps {
+    open: (event: React.MouseEvent<HTMLAnchorElement>) => void
+}
+
+/** TabPane props for user */
+export interface ITabPaneProps {
+    id: string
+    headerText: string
+    content: React.ReactNode
+}
+
+/** Private Sidebar TabPane props */
+export interface TabPaneProps extends ITabPaneProps {
+    close: () => void
+}
+
+/** Common TabPane props for user */
+export type PaneProps = ITabProps & ITabPaneProps
+
+/** Sidebar props */
+export interface SideBarProps {
+    id: string
+    tabs: PaneProps[]
+    position: SidebarPosition
+    settingsTab?: TabProps
+}
 
 /** Toolbar tab-button */
 const Tab: React.FC<TabProps> = (props: TabProps) => {
